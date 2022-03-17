StatiXOS/android_device_xiaomi_cannon

cannong: Initial makefiles
Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

AndroidProducts.mk
@@ -1,0 +1,15 @@
+#
+# Copyright (C) 2021 StatiXOS
+# Copyright (C) 2021 Vaisakh Murali
+#
+# SPDX-License-Identifer: GPL-2.0-only
+
+#
+# AOSP Lunch specification
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/statix_cannon.mk
+
+COMMON_LUNCH_CHOICES := \
+	statix_cannon-user

AOSP-on-Cannon/device_xiaomi_cannon

Change-Id: I17dd9a911860c2eab6b83e010986cc8c997e07bc

AndroidProducts.mk
@@ -1,0 +1,15 @@
+#
+# Copyright (C) 2021 StatiXOS
+# Copyright (C) 2021 Vaisakh Murali
+#
+# SPDX-License-Identifer: GPL-2.0-only
+
+#
+# AOSP Lunch specification
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/cannon.mk
+
+COMMON_LUNCH_CHOICES := \
+	cannon-user
96675bacff0441bc0928c299777f729adae21b1b
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-03 10:14:47 +0530

    cannong: Initial makefiles
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I17dd9a911860c2eab6b83e010986cc8c997e07bc

---
+++ cannon.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021 StatiXOS
+# Copyright (C) 2021 Vaisakh Murali
+#
+# SPDX-License-Identifer: GPL-2.0-only
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/cannon/device.mk)
+
+# Target specifications
+PRODUCT_NAME := cannon
+PRODUCT_DEVICE := cannon
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi Note 9T
+PRODUCT_MANUFACTURER := xiaomi
