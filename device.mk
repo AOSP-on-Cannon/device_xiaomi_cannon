5a2a19fede0aa84c46c758747355b30fc46b6e20
Vaisakh Murali <mvaisakh@statixos.com>
2022-03-28 13:27:44 +0600

    cannong: Build custom fingerprint AIDL HAL
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Ie2f03c30ab2eba940f5f8a229b87a636195903e2

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint-service.cannon \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
 # Gatekeeper
